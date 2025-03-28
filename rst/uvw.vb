40099de34705324718080c1763cae8cc43443843
unknownsaying <15368104568@189.cn>
2025-03-28 11:22:58 +0800

    Update uvw.vb

---
+++ rst/uvw.vb
@@ -0,0 +1,10 @@
+Imports InventorySlot/Interface
+Imports InventorySlot/Namespace
+Interface
+     Me.MustInherit
+     Typeof Inherits
+End Interface
+NamespacE
+     Me.MustOverride
+     Typeof Overrides
+End Namespace
