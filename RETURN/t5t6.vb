63941f65ab1267243e6be8e1d2f59c8b9c68bade
unknownsaying <15368104568@189.cn>
2025-03-28 12:50:25 +0800

    Update t5t6.vb

--- RETURN/t5t6.vb
+++ RETURN/t5t6.vb
@@ -1,7 +1,7 @@
 Imports System.Math
 Imports ijk
 Imports ijkxyz
-Imports ijkyzt
+Imports ijkxyzt
 Function t5t6
 return i / asin(wx + t5) + j / acos(wx - t6) + k / asin(wx + t5)
 return i / acos(wx - t5) + j / asin(wx + t6) + k / acos(wx - t5)
