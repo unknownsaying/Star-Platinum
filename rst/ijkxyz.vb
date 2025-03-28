8ddf352c3528174dac7e607803ee1303dd9f9f7b
unknownsaying <15368104568@189.cn>
2025-03-28 11:51:59 +0800

    Update ijkxyz.vb

--- rst/ijkxyz.vb
+++ rst/ijkxyz.vb
@@ -1,6 +1,9 @@
 Imports System.Math
+Declare Module x1{}
+Declare Module y1{}
+Declare Module x2{}
+Declare Module y2{}
 Function ijkxyz
-
      ijk = sin(i) * (x1 + x2) + cos(j) * (y1 + y2)
      ijk = cos(j) * (x1 - x2) + sin(i) * (y1 - y2)
      ijk = sin(i) * (x1 + x2) + sin(j) * (y1 + y2) + sin(k) * (z1 + z2)
