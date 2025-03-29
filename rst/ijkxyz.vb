395fcd47440986eedb940e2b6953c344c2051c41
unknownsaying <15368104568@189.cn>
2025-03-29 15:07:06 +0800

    Update ijkxyz.vb

--- rst/ijkxyz.vb
+++ rst/ijkxyz.vb
@@ -8,5 +8,4 @@ Function ijkxyz
      ijk = cos(j) * (x1 - x2) + sin(i) * (y1 - y2)
      ijk = sin(i) * (x1 + x2) + sin(j) * (y1 + y2) + sin(k) * (z1 + z2)
      ijk = cos(i) * (x1 + x2) + cos(j) * (y1 + y2) + cos(k) * (z1 + z2)
-
 End Function
