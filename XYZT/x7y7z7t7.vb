d0f804c69b4c544058dcd8d8140d4d894085c0ce
unknownsaying <15368104568@189.cn>
2025-03-29 15:06:10 +0800

    Update x7y7z7t7.vb

--- XYZT/x7y7z7t7.vb
+++ XYZT/x7y7z7t7.vb
@@ -4,6 +4,6 @@ Imports x3y3z3t3
 Imports x4y4z4t4
 Imports x5y5z5t5
 Imports x6y6z6t6
-Sub x7y7z7t7
+Select x7y7z7t7
     x1y1z1t1<->x2y2z2t2<->x3y3z3t3<->x4y4z4t4<->x5y5z5t5<->x6y6z6t6
-End Sub
+End Select
