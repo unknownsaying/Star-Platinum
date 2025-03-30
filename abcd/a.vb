268975581a0d72d3d437b0d0aba29e6c6ebb4736
unknownsaying <15368104568@189.cn>
2025-03-30 16:22:02 +0800

    Update a.vb

--- abcd/a.vb
+++ abcd/a.vb
@@ -1,9 +1,9 @@
 Dim AddHandler
 Sub a 
 Dim a1,a2,a3,a4 As Integer
- let a1 = 4
- let a2 = 3
- let a3 = 2
- let a4 = 1
+ let a1 = 1
+ let a2 = 2
+ let a3 = 3
+ let a4 = 4
 ReDim a = {a1,a2,a3,a4}
 End Sub
