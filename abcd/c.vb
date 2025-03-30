d78a75adc11ac807d48c7c60eb22d32083a1d4d4
unknownsaying <15368104568@189.cn>
2025-03-30 16:22:26 +0800

    Update c.vb

--- abcd/c.vb
+++ abcd/c.vb
@@ -1,9 +1,9 @@
 Dim RemoveHandler
 Sub c 
 Dim c1,c2,c3,c4 As Integer
- let c1 = 10
- let c2 = 1
- let c3 = 9
- let c4 = 8
+ let c1 = 12
+ let c2 = 13
+ let c3 = 14
+ let c4 = 15
 ReDim c = {c1,c2,c3,c4}
 End Sub
