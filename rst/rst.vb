1690e14075935e3937ccf22cea84afdd94886b42
unknownsaying <15368104568@189.cn>
2025-04-01 21:13:56 +0800

    Update rst.vb

--- rst/rst.vb
+++ rst/rst.vb
@@ -8,17 +8,17 @@ Imports XYZT/x4y4z4t4
 Imports XYZT/x5y5z5t5
 Imports XYZT/x6y6z6t6
 Imports XYZT/x7y7z7t7
-Function rst
+Function ijk
      i->j->k->t1
      j->k->i->t2
      k->l->j->t3
 End Function
-Function rst
+Function lmn
      l->m->n->t4
      m->n->l->t5
      n->l->m->t6
 End Function
-Function rst
+Function opq
      o->p->q->t7
      p->q->o->t7
      q->o->p->t7
