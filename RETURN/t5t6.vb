fadb4d198e6d43f144b77563396bd63691ee1836
unknownsaying <15368104568@189.cn>
2025-04-18 21:18:31 +0800

    Update t5t6.vb

--- RETURN/t5t6.vb
+++ RETURN/t5t6.vb
@@ -3,6 +3,6 @@ Imports ijk
 Imports ijkxyz
 Imports ijkxyzt
 Function t5t6
-return i / asin(wx + t5) + j / acos(wx - t6) + k / asin(wx + t5)
-return i / acos(wx - t5) + j / asin(wx + t6) + k / acos(wx - t5)
+return i / asin(wx + t5) + j / acos(wy - t6) + k / asin(wz + t5)
+return i / acos(wx - t5) + j / asin(wy + t6) + k / acos(wz - t5)
 End Function
