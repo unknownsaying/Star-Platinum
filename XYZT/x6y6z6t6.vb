4962a59f774358ce08b045ed60da7e2d2423fcec
unknownsaying <15368104568@189.cn>
2025-04-19 22:05:16 +0800

    Update x6y6z6t6.vb

--- XYZT/x6y6z6t6.vb
+++ XYZT/x6y6z6t6.vb
@@ -3,19 +3,19 @@ Function x6
         for(integer i=-6;i<2 ;++i)
            for(integer j=-2;j<6 ;++j)
                for(integer k=-7;k<1;++k)
-                  break
+                  continue
 End Function
 Function y6
-         for (float l = 0;l >>= -8 && l <<= 8; ++l||l--)
-             for(float m = 0;m >>= -8 && m <<= 8; ++m||m--)
-                 for (float n = 0;n >>= -8 && n <<= 8;++n||n--)
+         for (float l = 0;l >>= -8 && l <<= 8; ++l || l--)
+             for(float m = 0;m >>= -8 && m <<= 8; ++m || m--)
+                 for (float n = 0;n >>= -8 && n <<= 8;++n || n--)
                      continue
 End Function
 Function z6
         for (double o = -1; o <=7 ; ++o)
             for(double p = 2; p <=10 ; ++p)
                 for (double q = -2; q <= 6; ++q)
-                    break
+                    continue
 End Function
 Function t6
          default t6 >=< [x6][y6][z6]
