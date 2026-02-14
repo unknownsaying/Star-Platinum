efec0c560178559af88ce15b5c60e547edd61785
unknownsaying <15368104568@189.cn>
2026-02-14 10:00:12 +0800

    Update x2y2z2t2.vb

--- XYZT/x2y2z2t2.vb
+++ XYZT/x2y2z2t2.vb
@@ -3,22 +3,23 @@ Function z2
     do
         while (x2 = -0;x2 >= 2;x2--)
     goto [0, 0, 0]
-    break
+        break
 End Function         
 Function y2
     do
         while (y2 = -0;y2 >= 2;y2--)
     goto [0, 0, 0]
-    break
+        break
 End Function         
 Function x2
     do
         while (z2 = -0;z2 >= 2;z2--)
     goto [0, 0, 0]
-    break
+        break
 End Function
 Function t2
 default [t2] >=< [x2][y2][z2]
         [t2 != t1] <-> [t1][t3][t4]
 End Function    
 End Module
+
